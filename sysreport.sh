fd5353427dbedf01e4afa6b7b6773336d7e30b0a
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2026-04-18 21:15:53 +1000

    Add uptime and disk usage to sysreport script

--- sysreport.sh
+++ sysreport.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
 # Output one page system health summary
 # uptime
+echo "The system has been running since"
+uptime
 #
 # disk usage
+echo "The filesystem is using this much disk space"
+df -hc
 #
 # top 5 processes by memory
 #
