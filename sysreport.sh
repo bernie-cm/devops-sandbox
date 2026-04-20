4a014ae26651717243307a937f06dadbdd3a9643
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2026-04-20 20:57:21 +1000

    Write top down design of script

--- sysreport.sh
+++ sysreport.sh
@@ -2,22 +2,30 @@
 set -euo pipefail
 # sysreport.sh - A script to produce a one page health summary
 # Output one page system health summary
-# uptime
+title="System Information for"
+RIGHT_NOW="$(date +"%x %r %Z")"
+TIMESTAMP="Updated on $RIGHT_NOW by $USER"
 echo "System Heatlh Summary"
+echo $title $HOSTNAME
 
+# Write time stamp when the script was executed
+echo $TIMESTAMP
+# Write uptime
 echo "=== UPTIME ==="
 echo "The system has been running since"
 uptime
-#
-# disk usage
+
+# Write disk space
 echo "=== DISK USAGE ==="
 echo "The filesystem is using this much disk space"
 df -h --total
+# Write home space
 #
-# top 5 processes by memory
+# Write top 5 processes by memory
 #
-# listening ports
+# Write listening ports
 #
-# failed login attempts
+# Write failed login attempts
 #
-# last 10 sudo invocations
+# Write last 10 sudo invocations
+#
