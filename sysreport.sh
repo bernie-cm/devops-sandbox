1dc94b72dda6680e259e57d4fa55dd1d43338c7f
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2026-04-20 21:20:06 +1000

    Add uptime, and disk usage functions

--- sysreport.sh
+++ sysreport.sh
@@ -8,31 +8,31 @@ RIGHT_NOW="$(date +"%x %r %Z")"
 TIMESTAMP="Updated on $RIGHT_NOW by $USER"
 
 ##### Functions
-function (show_uptime) {
-
+function show_uptime {
+  echo "=== System uptime ==="
+  uptime
 }
 
-function (drive_space) {
-
+function drive_space {
+  echo "=== Filesystem space ==="
+  df
 }
 
-function (home_space) {
-
+function top_processes_memory {
+  # Use $1 to pass the number of top processes
+  echo "function top_processes_memory"
 }
 
-function (top_processes_memory(n)) {
-
+function listening_ports {
+  echo "function listening_ports"
+}
+function show_failed_logins {
+  echo "function show_failed_logins"
 }
 
-function (listening_ports) {
-
-}
-function (show_failed_logins) {
-
-}
-
-function (last_sudo_invocations(n)) {
-
+function last_sudo_invocations {
+  # Use $1 to pass the number of sudo invocations desired
+  echo "function last_sudo_invocations"
 }
 
 ##### Main
@@ -41,28 +41,19 @@ echo $TITLE $HOSTNAME
 
 # Write time stamp when the script was executed
 echo $TIMESTAMP
-$(show_uptime)
 # Write uptime
-echo "=== UPTIME ==="
-echo "The system has been running since"
-uptime
-
+show_uptime
 # Write disk space
-$(drive_space)
-echo "=== DISK USAGE ==="
-echo "The filesystem is using this much disk space"
-df -h --total
-# Write home space
-$(home_space)
+drive_space
 #
 # Write top 5 processes by memory
-$(top_processes_memory(5))
+top_processes_memory 5
 
 # Write listening ports
-$(listening_ports)
+listening_ports
 
 # Write failed login attempts
-$(show_failed_logins)
+show_failed_logins
 
 # Write last 10 sudo invocations
-$(last_sudo_invocations(10))
+last_sudo_invocations 10
